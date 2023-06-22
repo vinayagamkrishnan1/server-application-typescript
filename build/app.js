ccce2b6287c8990cb5baf319ace954a148fdb6d8
Vinay K <vinayboi01@gmail.com>
2023-06-22 12:10:10 +0530

    correction for express middlewares

--- build/app.js
+++ build/app.js
@@ -4,17 +4,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const compression_1 = __importDefault(require("compression"));
+const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
 // import userRoutes from "../src/routes/users";
 // import fcmNotificationRoutes from "../src/routes/users";
 // import firebaseAdminNotificationRoutes from "../src/routes/users";
 const app = (0, express_1.default)();
+// Express middlewares
+app.use((0, morgan_1.default)("dev"));
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
+app.use(express_1.default.json());
+app.use((0, compression_1.default)());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, cookie_parser_1.default)());
+app.use((0, helmet_1.default)());
+// app.use(express.static(path.join(__dirname, "public")));
+app.use((0, cors_1.default)());
 app.use("/", (req, res) => {
     res.send('Hello world!');
 });
 // app.use("/user/api", userRoutes);
 // app.use("/notification/fcm", fcmNotificationRoutes);
 // app.use("/notification/firebaseadmin", firebaseAdminNotificationRoutes);
-// app.listen(PORT, (): void => {
-//     console.log('SERVER IS UP ON PORT:', PORT);
-// });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import logger from "morgan";
+import helmet from "helmet";
+import cors from "cors";
 
 // import userRoutes from "../src/routes/users";
 // import fcmNotificationRoutes from "../src/routes/users";
 // import firebaseAdminNotificationRoutes from "../src/routes/users";
 
 const app: Application = express();
+
+// Express middlewares
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(compression());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(helmet());
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", (req: Request, res: Response): void => {
     res.send('Hello world!');
@@ -14,8 +32,5 @@ app.use("/", (req: Request, res: Response): void => {
 // app.use("/notification/fcm", fcmNotificationRoutes);
 // app.use("/notification/firebaseadmin", firebaseAdminNotificationRoutes);
 
-// app.listen(PORT, (): void => {
-//     console.log('SERVER IS UP ON PORT:', PORT);
-// });
 
 export default app;
