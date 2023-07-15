b9ad142bab66b52de2998803fcff27ebc43dfec3
Vinay K <vinayboi01@gmail.com>
2023-07-15 10:59:23 +0530

    correction

--- src/routes/notificationfirebaseadmin/notificationfirebaseadmin.ts
+++ src/routes/notificationfirebaseadmin/notificationfirebaseadmin.ts
@@ -2,7 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import { firebaseAdminNotificationController } from "../../controllers/index";
 const router = express.Router();
 
-router.post('/send', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/send', (req: Request, res: Response, next: NextFunction) => {
   firebaseAdminNotificationController.sendNotification(req, res, next);
 });
 
--- src/routes/user/user.ts
+++ src/routes/user/user.ts
@@ -2,15 +2,15 @@ import express, { NextFunction, Request, Response } from "express";
 import { userController } from "../../controllers/index";
 const router = express.Router();
 
-router.post('/create', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/create', (req: Request, res: Response, next: NextFunction) => {
   userController.createUser(req, res, next);
 });
 
-router.put('/update', async (req: Request, res: Response, next: NextFunction) => {
+router.put('/update', (req: Request, res: Response, next: NextFunction) => {
   userController.updateUser(req, res, next);
 });
 
-router.delete('/delete', async (req: Request, res: Response, next: NextFunction) => {
+router.delete('/delete', (req: Request, res: Response, next: NextFunction) => {
   userController.deleteUser(req, res, next);
 });
 
